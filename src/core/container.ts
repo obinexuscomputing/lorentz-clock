408341fe3231a59330cf276ed07c3e928a1d2b0e
Nnamdi Michael Okpala <obinexus@tuta.com>
2024-12-05 13:42:46 +0000

    inti

--- src/core/container.ts
+++ src/core/container.ts
@@ -1,49 +1,56 @@
 import 'reflect-metadata';
-import type { interfaces } from 'inversify';
 import { Container as InversifyContainer } from 'inversify';
 import { TYPES } from '../types/injection-tokens';
-
-// Import service interfaces
 import { IGeolocationService } from '../geolocation/api';
 import { ILocationTransformService } from '../geolocation/location-transform';
 import { IGeoUtilsService } from '../geolocation/geo-utils';
-
-// Import service implementations
 import { GeolocationService } from '../geolocation/api';
 import { LocationTransformService } from '../geolocation/location-transform';
 import { GeoUtilsService } from '../geolocation/geo-utils';
 
 export class ContainerProvider {
-    private static instance: InversifyContainer;
+    private static instance: InversifyContainer | undefined;
 
     public static getInstance(): InversifyContainer {
         if (!ContainerProvider.instance) {
-            const container = new InversifyContainer({ defaultScope: "Singleton" });
-            
-            // Register core services
-            container.bind<IGeolocationService>(TYPES.GeolocationService)
-                    .to(GeolocationService)
-                    .inSingletonScope();
-                    
-            container.bind<ILocationTransformService>(TYPES.LocationTransformService)
-                    .to(LocationTransformService)
-                    .inSingletonScope();
-                    
-            container.bind<IGeoUtilsService>(TYPES.GeoUtilsService)
-                    .to(GeoUtilsService)
-                    .inSingletonScope();
-
-            ContainerProvider.instance = container;
+            ContainerProvider.instance = ContainerProvider.createContainer();
         }
-
         return ContainerProvider.instance;
     }
 
+    private static createContainer(): InversifyContainer {
+        const container = new InversifyContainer({ 
+            defaultScope: "Singleton",
+            skipBaseClassChecks: false
+        });
+        
+        // Register core services
+        container.bind<IGeolocationService>(TYPES.GeolocationService)
+                .to(GeolocationService)
+                .inSingletonScope();
+                
+        container.bind<ILocationTransformService>(TYPES.LocationTransformService)
+                .to(LocationTransformService)
+                .inSingletonScope();
+                
+        container.bind<IGeoUtilsService>(TYPES.GeoUtilsService)
+                .to(GeoUtilsService)
+                .inSingletonScope();
+
+        return container;
+    }
+
     public static resetContainer(): void {
-        ContainerProvider.instance = null;
+        if (ContainerProvider.instance) {
+            ContainerProvider.instance.unbindAll();
+            ContainerProvider.instance = undefined;
+        }
+    }
+
+    public static hasInstance(): boolean {
+        return ContainerProvider.instance !== undefined;
     }
 }
 
 export const container = ContainerProvider.getInstance();
-
 export type Container = InversifyContainer;
