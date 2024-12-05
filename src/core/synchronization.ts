62d7f4d64750bf9bbadb2681bfaa1c49746bb6cc
Nnamdi Michael Okpala <obinexus@tuta.com>
2024-12-05 13:06:25 +0000

    init

--- src/core/synchronization.ts
+++ src/core/synchronization.ts
@@ -1,6 +1,7 @@
 import { calculateLorentzFactor } from './lorentz';
 import { calculateTimeDilation } from './time-dilation';
-import { Coordinates, RelativeVelocity, TimeInterval } from '../types/core';
+import { Coordinates, RelativeVelocity, TimeInterval } from '@types/';
+import { SPEED_OF_LIGHT } from './lorentz';
 
 /**
  * Represents a spacetime event with both spatial and temporal coordinates
@@ -12,24 +13,15 @@ interface SpacetimeEvent {
 
 /**
  * Calculates the simultaneous events in a moving reference frame
- * 
- * @param event - The reference event
- * @param velocity - Relative velocity between frames
- * @returns Adjusted spacetime coordinates for simultaneous events
  */
 export function calculateSimultaneousEvents(
     event: SpacetimeEvent,
     velocity: RelativeVelocity
 ): SpacetimeEvent[] {
     const lorentzFactor = calculateLorentzFactor(velocity);
-    
-    // Calculate time dilation for the event
     const dilatedTime = calculateTimeDilation(event.time, velocity);
-    
-    // Calculate length contraction
     const contractionFactor = 1 / lorentzFactor;
     
-    // Generate a series of simultaneous events
     const simultaneousEvents: SpacetimeEvent[] = [];
     
     // Calculate events at different spatial points that are simultaneous in the moving frame
@@ -54,11 +46,6 @@ export function calculateSimultaneousEvents(
 
 /**
  * Synchronizes clocks between two locations considering relativistic effects
- * 
- * @param location1 - First location coordinates
- * @param location2 - Second location coordinates
- * @param velocity - Relative velocity between locations
- * @returns Time offset needed for synchronization
  */
 export function synchronizeClocks(
     location1: Coordinates,
@@ -67,14 +54,12 @@ export function synchronizeClocks(
 ): TimeInterval {
     const lorentzFactor = calculateLorentzFactor(velocity);
     
-    // Calculate spatial separation
     const separation = Math.sqrt(
         Math.pow(location2.x - location1.x, 2) +
         Math.pow(location2.y - location1.y, 2) +
         Math.pow(location2.z - location1.z, 2)
     );
     
-    // Calculate time offset due to relativistic effects
     const timeOffset = (velocity * separation) / Math.pow(SPEED_OF_LIGHT, 2);
     
     return timeOffset * lorentzFactor;
